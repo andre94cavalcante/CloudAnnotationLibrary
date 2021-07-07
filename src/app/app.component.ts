372adbda30a2feb652f4c2f93d76563afe8132e8
andre94cavalcante <andre.94.cavalcante@gmail.com>
2021-07-07 09:33:25 -0300

    env angular prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
