7fb965878cf89ccf26a954c934cb6755b1b2daee
andre94cavalcante <andre.94.cavalcante@gmail.com>
2021-07-31 11:53:34 -0300

    responsiveness

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -3,32 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
-
 export class TopbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-
-    const topbar = document.getElementById("myTopnav")
-    const el = document.getElementById("hamburger");
-    el.addEventListener("click", responsiveHamburger);
-
-
-    function responsiveHamburger() {
-      if (el.className === "icon") {
-        el.className += " active";
-        topbar.className += " responsive"
-      } else {
-        el.className = "icon";
-        topbar.className = "topnav"
-      }
+  responsiveHamburger = () => {
+    let topbar = document.getElementById('myTopnav');
+    let el = document.getElementById('hamburger');
+    if (el.className === 'icon') {
+      el.className += ' active';
+      topbar.className += ' responsive';
+    } else {
+      el.className = 'icon';
+      topbar.className = 'topnav';
     }
-  }
+  };
 
+  ngOnInit(): void {}
 }
-
